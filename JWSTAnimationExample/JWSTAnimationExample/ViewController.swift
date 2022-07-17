d082397848546be5d3d27abdf84043e5ec954123
Nikita Gayko <nik.gayko@gmail.com>
2022-07-17 16:37:28 +0300

    Initial iOS project setup

---
+++ JWSTAnimationExample/JWSTAnimationExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JWSTAnimationExample
+//
+//  Created by Nikita Gayko on 17.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
